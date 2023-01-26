ac6a6616a7776b5f603f67dd97ab3eb3c2952ec9
IronWil1 <aidarziyangirov@gmail.com>
2023-01-26 15:40:49 +0300

    first commit

---
+++ account-service/src/main/java/com/microservices/account/AccountApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.account;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountApplication {
+    public static void main(String[] args) {
+
+    }
+}
